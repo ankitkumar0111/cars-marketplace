df845ed0f1ca6409cfb36463febaaf152fd36d1a
ankitkumar0111 <ankit324kumar@gmail.com>
2023-11-12 00:18:30 +0530

    Project Completed

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -6,14 +6,13 @@ import React from "react";
 import { CustomButton } from ".";
 import { updateSearchParams } from "@/utils";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-  const router = useRouter();
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
+  
 
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
-    const newPathName = updateSearchParams("limit", `${newLimit}`)
-
-    router.push(newPathName)
+   
+    setLimit(newLimit)
   };
 
   return (
